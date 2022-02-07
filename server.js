6e2874c531a7180baae829da77a3c1cd3c2bb811
MarcusKri <marcus-kristoffersen@outlook.com>
2022-02-07 18:48:53 +0100

    respond to POST

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/login", (req, res) => {
     res.json({
@@ -8,9 +10,23 @@ app.get("/login", (req, res) => {
     });
 })
 
+const users = [
+    {
+        username: "admin", password: "321terces"
+    }
+]
+
+
 app.post("/login", (req, res) => {
-    // set something so that GET /login returns username
-    res.end()
+    const body = req.body;
+    const username = body.username;
+    const password = body.password;
+
+    if (users.find(u => u.username === username).password === password){
+        res.sendStatus(200)
+    } else {
+        res.send(401);
+    }
 })
 
 const server = app.listen(process.env.PORT || 3000, () => {
